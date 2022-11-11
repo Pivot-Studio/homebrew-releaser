7aae48769d12161d76f337010ac9161dd7527763
Boxiu Lee <49905799+Chronostasys@users.noreply.github.com>
2022-11-12 00:21:56 +0800

    Update formula.py

--- homebrew_releaser/formula.py
+++ homebrew_releaser/formula.py
@@ -160,7 +160,16 @@ class {{class_name}} < Formula
     {{{install_instructions}}}
   end
   {{# test_instructions}}
-
+  def caveats
+    s = <<~EOS
+    \033[0;32m安装完成，请运行下方命令添加环境变量\033[0m
+      echo 'export KAGARI_LIB_ROOT=$(brew --prefix pivot-lang)/lib/planglib' >> ~/.bash_profile
+      echo 'export PL_ROOT=$(brew --prefix pivot-lang)/lib' >> ~/.bash_profile
+      source ~/.bash_profile
+    \033[0;32m如果您已经打开了vsc，可能需要重启它来应用环境变量更改\033[0m
+    EOS
+    s
+  end
   test do
     {{{test_instructions}}}
   end
